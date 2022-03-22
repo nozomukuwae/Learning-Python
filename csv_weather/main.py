f4dec645290e9d6015d9186216bfdb921b622cc6
Nozomu Kuwae <nozomukuwae@gmail.com>
2022-03-22 17:47:13 +0900

    [csv_weather] read csv using panda

--- csv_weather/main.py
+++ csv_weather/main.py
@@ -1,7 +1,4 @@
-import csv
+import pandas
 
-with open("weather_data.csv") as file:
-    full_data = csv.reader(file)
-    data = list(full_data)[1:]
-    temperatures = list(map(lambda row: row[1], data))
-    print(temperatures)
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
